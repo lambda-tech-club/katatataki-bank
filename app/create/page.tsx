961058c0ce2b5c1120001ff3d0db0cd9270de9e3
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-04-16 11:27:31 +0000

    refactor: remove not needed import

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FormEvent, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { renderSVG } from 'uqr'
